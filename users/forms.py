ee2a90fc27a6480ad0e0ca334d43798a86e6d4de
Maksim Tyrin <makstyrin@mail.ru>
2025-09-28 18:36:36 +0800

    Окончена

--- users/forms.py
+++ users/forms.py
@@ -4,39 +4,55 @@ from django.contrib.auth import get_user_model, authenticate
 from django.utils.html import strip_tags
 from django.core.validators import RegexValidator
 
+
 User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True, max_length=66,
-                                widget=forms.EmailInput(attrs={'class': 'input-register form-control', 'placeholder': 'Ваша почта'}))
-    first_name = forms.CharField(required=True, max_length=50,
-                                widget=forms.EmailInput(attrs={'class': 'input-register form-control', 'placeholder': 'Ваше имя'}))
-    last_name = forms.CharField(required=True, max_length=50,
-                                widget=forms.EmailInput(attrs={'class': 'input-register form-control', 'placeholder': 'Ваше фамилия'}))
-    password1 = forms.CharField(required=True,
-                                widget=forms.PasswordInput(attrs={'class': 'input-register form-control', 'placeholder': 'Ваш пароль'}))
-    password2 = forms.CharField(required=True,
-                                widget=forms.PasswordInput(attrs={'class': 'input-register form-control', 'placeholder': 'Подтвердите пароль'}))
-    marketing_consent1 = forms.BooleanField(required=False,
-                                label='Я согласен получать коммерческие, рекламные и маркетинговые сообщения.',
-                                widget = forms.CheckboxInput(attrs={'class': 'checkbox-input-register'}))
-    marketing_consent2 = forms.BooleanField(required=False,
-                                label='Я согласен получать персонализированные коммерческие сообщения.',
-                                widget = forms.CheckboxInput(attrs={'class': 'checkbox-input-register'}))
-
-
+    email = forms.EmailField(
+        required=True,
+        max_length=66,
+        widget=forms.EmailInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your email'})
+    )
+    first_name = forms.CharField(
+        required=True,
+        max_length=50,
+        widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your first name'})
+    )
+    last_name = forms.CharField(
+        required=True,
+        max_length=50,
+        widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your last name'})
+    )
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your password'})
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'input-register form-control', 'placeholder': 'Confirm your password'})
+    )
+    marketing_consent1 = forms.BooleanField(
+        required=False,
+        label="I agree to receive commercial, promotional, and marketing communications.",
+        widget=forms.CheckboxInput(attrs={'class': 'checkbox-input-register'})
+    )
+    marketing_consent2 = forms.BooleanField(
+        required=False,
+        label="I agree to receive personalized commercial communications.",
+        widget=forms.CheckboxInput(attrs={'class': 'checkbox-input-register'})
+    )
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'password1', 'password2'
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2',
                   'marketing_consent1', 'marketing_consent2')
-        
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('Этот электронный адрес уже используется.')
+            raise forms.ValidationError('This email is already in use.')
         return email
-    
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.username = None
@@ -46,38 +62,50 @@ class CustomUserCreationForm(UserCreationForm):
             user.save()
         return user
     
-class CustomUserLoginForm(AuthenticationForm):
-    username = forms.CharField(label='Email',
-                               widget=forms.TextInput(attrs={'autofocus':True, 'class': 'input-register form-control', 'placeholder': 'Ваша почта'}))
 
-    password = forms.CharField(label='Password',
-                               widget=forms.TextInput(attrs={'autofocus':True, 'class': 'input-register form-control', 'placeholder': 'Ваш пароль'}))
+class CustomUserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Email',
+        widget=forms.TextInput(attrs={'autofocus': True, 'class': 'input-register form-control', 'placeholder': 'Your email'})
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your Password'})
+    )
 
     def clean(self):
         email = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
 
         if email and password:
-            self.user_cache = authenticate(self. request, username=email, password=password)
+            self.user_cache = authenticate(self.request, username=email, password=password)  # Используйте username вместо email
             if self.user_cache is None:
-                raise forms.ValidationError('Неверный адрес электронной почты или пароль.')
+                raise forms.ValidationError('Invalid email or password.')
             elif not self.user_cache.is_active:
-                raise forms.ValidationError('Эта учетная запись неактивна.')
+                raise forms.ValidationError('This account is inactive.')
         return self.cleaned_data
     
+
 class CustomUserUpdateForm(forms.ModelForm):
-    phone = forms.CharField(required=False,
-                            validators=[RegexValidator(r'^/+?1?/d{9,15}$', 'Введите действительный номер телефона.')],
-                            widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Ваш номер телефона'}))
-
-    first_name = forms.CharField(required=True, max_length=50,
-                                widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Ваше имя'}))
-
-    last_name = forms.CharField(required=True, max_length=50,
-                                widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Ваша фамилия'}))
-                
-    email = forms.EmailField(required=False,
-                            widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Ваша почта'}))
+    phone = forms.CharField(
+        required=False,
+        validators=[RegexValidator(r'^\+?1?\d{9,15}$', "Enter a valid phone number.")],
+        widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your phone number'})
+    )
+    first_name = forms.CharField(
+        required=True,
+        max_length=50,
+        widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your first name'})
+    )
+    last_name = forms.CharField(
+        required=True,
+        max_length=50,
+        widget=forms.TextInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your last name'})
+    )
+    email = forms.EmailField(
+        required=False,
+        widget=forms.EmailInput(attrs={'class': 'input-register form-control', 'placeholder': 'Your email'})
+    )
 
 
     class Meta:
@@ -85,40 +113,42 @@ class CustomUserUpdateForm(forms.ModelForm):
         fields = ('first_name', 'last_name', 'email', 'address1', 'address2',
                   'city', 'country', 'province', 'postal_code', 'phone')
         widgets = {
-            'email': forms.EmailInput(attrs={'class': 'input-register form-control',
-                                            'placeholder': 'Ваша почта'}),
-            'first_name': forms.TextInput(attrs={'class': 'input-register form-control',
-                                                 'placeholder': 'Ваше имя'}),
-            'last_name': forms.TextInput(attrs={'class': 'input-register form-control',
-                                                 'placeholder': 'Ваша фамилия'}),
-            'address1': forms.TextInput(attrs={'class': 'input-register form-control',
-                                                 'placeholder': 'Ваш адрес 1'}),
-            'address2': forms.TextInput(attrs={'class': 'input-register form-control',
-                                                 'placeholder': 'Ваш адрес 2'}),
-            'city': forms.TextInput(attrs={'class': 'input-register form-control',
-                                                 'placeholder': 'Ваш город'}),
-            'country': forms.TextInput(attrs={'class': 'input-register form-control',
-                                                 'placeholder': 'Ваша страна'}),
-            'province': forms.TextInput(attrs={'class': 'input-register form-control',
-                                                 'placeholder': 'Ваша провинция'}),
-            'postal_code': forms.TextInput(attrs={'class': 'input-register form-control',
-                                                 'placeholder': 'Ваш почтовый индекс'}),
+            'email': forms.EmailInput(attrs={'class': 'input-register form-control', 
+                                             'placeholder': 'Your email'}),
+            'first_name': forms.TextInput(attrs={'class': 'input-register form-control', 
+                                                 'placeholder': 'Your first name'}),
+            'last_name': forms.TextInput(attrs={'class': 'input-register form-control', 
+                                                'placeholder': 'Your last name'}),
+            'address1': forms.TextInput(attrs={'class': 'input-register form-control', 
+                                               'placeholder': 'Address line 1'}),
+            'address2': forms.TextInput(attrs={'class': 'input-register form-control', 
+                                               'placeholder': 'Address line 2'}),
+            'city': forms.TextInput(attrs={'class': 'input-register form-control', 
+                                           'placeholder': 'Your city'}),
+            'country': forms.TextInput(attrs={'class': 'input-register form-control', 
+                                              'placeholder': 'Your country'}),
+            'province': forms.TextInput(attrs={'class': 'input-register form-control', 
+                                               'placeholder': 'Your province'}),
+            'postal_code': forms.TextInput(attrs={'class': 'input-register form-control', 
+                                                  'placeholder': 'Your postal code'}),
         }
 
+    
     def clean_email(self):
-        email = self.changed_data.get('email')
-        if email and User.objects.filter(email=email).exclude(id-self.instance.id).exists():
-            raise forms.ValidationError('Это электронное письмо уже используется.')
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exclude(id=self.instance.id).exists():
+            raise forms.ValidationError('This email is already in use.')
         return email
     
+
     def clean(self):
         cleaned_data = super().clean()
         if not cleaned_data.get('email'):
             cleaned_data['email'] = self.instance.email
 
-            for field in ['address1', 'address2', 'city', 'country', 'province',
-                          'postal-code', 'phone']:
-                if cleaned_data.get(field):
-                    cleaned_data[field] = strip_tags(cleaned_data[field])
+        for field in ['address1', 'address2', 'city', 'country', 'province',
+                        'postal_code', 'phone']:
+            if cleaned_data.get(field):
+                cleaned_data[field] = strip_tags(cleaned_data[field])
 
-                return cleaned_data
+        return cleaned_data
